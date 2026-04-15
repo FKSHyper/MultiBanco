0e190ba7c64eb6ffe69c67e73ccff088324d58ab
FKSHyper <128801212+FKSHyper@users.noreply.github.com>
2026-04-15 02:09:35 +0100

    Final - Acho

--- main.py
+++ main.py
@@ -3,26 +3,6 @@ import admin
 import utilizador
 from utilitarios import realizar_login
 
-# Estrutura Principal
-
-# !!Estrutura de Dados!!#
-banco_dados = {
-    "1001": {
-        "nome": "João Silva",
-        "pin": "1234",
-        "saldo": 1000.0,
-        "movimentos": [
-            {"data": "2023-10-27 10:30", "tipo": "Abertura", "valor": 1000.0, "destino": "-"}
-        ]
-    },
-    "1002": {
-        "nome": "Maria Santos",
-        "pin": "4321",
-        "saldo": 1000.0,
-        "movimentos": []
-    }
-}
-
 # MAIN
 def main():
     contas = dados.carregar_dados()
